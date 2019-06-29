7316f035ed609e57670f8a46721befdfc65f4298
VINAMRA SHREY <19341012+vs47096@users.noreply.github.com>
2019-06-30 01:27:39 +0530

    Finalized main method body

--- Spring-REST-CRUD-Application/src/main/java/com/home/main/App.java
+++ Spring-REST-CRUD-Application/src/main/java/com/home/main/App.java
@@ -1,18 +1,36 @@
 package com.home.main;
 
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.home.configuration.SpringConfig;
 import com.home.model.Book;
+import com.home.resources.AppConfig;
+import com.home.resources.LogConfig;
 
-public class App {
+public class App 
+{
+	final static Logger LOGGER = LogConfig.getLogger(App.class);
+	
 	public static void main(String[] args) {
-		System.out.println("Entered main");
+		
+		System.out.println("Program Execution Started");
+		System.out.println("***************************************************");
 		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringConfig.class);
+		System.out.println("Application Context Created and Loaded");
+		LOGGER.info("Context creation done");
 		System.out.println("Created application context");
 		Book book = applicationContext.getBean(Book.class);
-		System.out.println("Got Bean");
-		System.out.println(book);
+		System.out.println("Got Bean of type Book and bean object is - "+book);
+		LOGGER.info("Bean retrieval done");
+		System.out.println("***************************************************");
+		System.out.println("Now it`s time to access values from properties file");
+		System.out.println("***************************************************");
+		System.out.println(AppConfig.PROPERTIES.getProperty("Database.USERNAME"));
+		System.out.println(AppConfig.PROPERTIES.getProperty("LoginApi.LOGIN_FAILURE"));
+		LOGGER.info("Values from properties file accessed");
+		System.out.println("***************************************************");
+		System.out.println("Program Execution Completed");
 	}
 }
