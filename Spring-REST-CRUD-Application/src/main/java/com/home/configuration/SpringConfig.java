42afeef64d034b5dc61bfdbad2eb3b7fddfba69b
VINAMRA SHREY <19341012+vs47096@users.noreply.github.com>
2019-07-03 00:25:33 +0530

    Added Hibernate dependency

--- Spring-REST-CRUD-Application/src/main/java/com/home/configuration/SpringConfig.java
+++ Spring-REST-CRUD-Application/src/main/java/com/home/configuration/SpringConfig.java
@@ -8,7 +8,8 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import com.home.model.Book;
 
 @Configuration
-@EnableAspectJAutoProxy
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+
 @ComponentScan(basePackages = "com.home.service com.home.dao com.home.utiltiy")
 public class SpringConfig 
 {
