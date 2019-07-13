3bcb2d30369d3240f50e74656c7ff90817cbc41c
VINAMRA SHREY <19341012+vs47096@users.noreply.github.com>
2019-07-14 02:27:59 +0530

    Implemented TestSuite class to run all test cases

---
+++ Spring-REST-CRUD-Application/src/test/java/com/home/testsuite/SpringRestCrudTestSuite.java
@@ -0,0 +1,28 @@
+package com.home.testsuite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.home.dao.AuthorDAOTest;
+import com.home.dao.LoginDAOTest;
+import com.home.service.AuthorServiceTest;
+import com.home.service.LoginServiceTest;
+import com.home.validator.AuthorValidatorTest;
+import com.home.validator.BookValidatorTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	
+	AuthorValidatorTest.class,
+	BookValidatorTest.class,
+	
+	LoginDAOTest.class,
+	AuthorDAOTest.class,
+	
+	LoginServiceTest.class,
+	AuthorServiceTest.class
+})
+public class SpringRestCrudTestSuite {
+
+}
